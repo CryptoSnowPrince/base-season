9ac470374c332633ebca986db0efdad3cead7530
CryptoSnowPrince <topdirector2017@gmail.com>
2024-11-11 19:09:54 +0000

    update utils

--- smart-contracts/scripts/utils.js
+++ smart-contracts/scripts/utils.js
@@ -2,6 +2,13 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function sendTransaction(account, to, value) {
+    const tx = await account.sendTransaction({ to, value })
+    const ret = await tx.wait()
+    console.log("tx ret: ", ret.transactionHash)
+}
+
 module.exports = {
-    delay
+    delay,
+    sendTransaction
 }
